b13e861da1c7eff1e5bc5c2feedcc26b6fa49188
Kathy White <kawhite8503@gmail.com>
2022-06-08 11:52:39 -0500

    add contact info

--- js/app.js
+++ js/app.js
@@ -1,2 +1,24 @@
-import { projectData } from "./project-data"; 
+import
+{projectData} from "./project-data"; 
 
+window.onscroll = function() {scrollFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function scrollFunction() {
+if (window.pageYOffset >= sticky) {
+  navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// add github icon and linked in icon to nav bar
+//get navbar to scroll with page
+//fix picture styling - smaller, centered on page
+//figure out how to put mission statement on top of picture, centered
+//type out About section
+//add card(s) for work section
+//update resume
+//attach resume to button?
