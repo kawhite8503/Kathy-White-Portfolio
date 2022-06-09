e17b527e82a060f2329405385ff40afe2115dd3d
Kathy White <kawhite8503@gmail.com>
2022-06-09 14:53:54 -0500

    respond to mobile sizing - work section

--- js/app.js
+++ js/app.js
@@ -20,7 +20,9 @@ let projectMarkup = projectData.map(project =>
 
 cardContainer.innerHTML = projectMarkup
 
-const resumeBtn = document.querySelector('#resume-button')
+
+
+// const resumeBtn = document.querySelector('#resume-button')
 
 // resumeBtn.addEventListener('click', play())
 
@@ -30,46 +32,10 @@ const resumeBtn = document.querySelector('#resume-button')
 // }
 
 
-// window.onscroll = function() {scrollFunction()};
-
-// var navbar = document.getElementById("navbar");
-// var sticky = navbar.offsetTop;
-
-// function scrollFunction() {
-// if (window.pageYOffset >= sticky) {
-//   navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
-
-
-// let wholeNav = document.querySelector('.whole-nav');
-// let sticky = wholeNav.offsetTop;
-
-
-
-// window.onscroll = function(){
-//   stickyNav()
-// }
-
-// function stickyNav (){
-//   if (window.pageYOffset > sticky) {
-//     wholeNav.classList.add("fixed")
-//   } else {
-//     wholeNav.classList.remove("fixed")
-//   }
-// }
-
-
-
-// add github icon and linked in icon to nav bar
-//get navbar to scroll with page
 //figure out how to put mission statement on top of picture, centered
 //type out About section
 //edit cards
-//update resume
-//attach resume to button?
+
+
 
 
