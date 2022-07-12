4acaf595f0eea391db39942b544ba34a1398ac65
Kathy White <kawhite8503@gmail.com>
2022-07-12 00:48:52 -0500

    add target_blank to links

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@ let projectMarkup = projectData.map(project =>
   <div class="card-body">
     <h5 class="card-title">${project.title}</h5>
     <p class="card-text">${project.description}</p>
-    <a href="${project.github}" class="btn btn-primary">GitHub</a>
-    <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
+    <a href="${project.github}" target="_blank" class="btn btn-primary">GitHub</a>
+    <a href="${project.deployment}" target="_blank" class="btn btn-secondary">Deployment</a>
   </div>
 </div>
   `).join(' ')
