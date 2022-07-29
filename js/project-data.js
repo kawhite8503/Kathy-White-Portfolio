9a0514b1d046d8316be3090ea94df7089b97b786
Kathy White <kawhite8503@gmail.com>
2022-07-28 22:10:07 -0500

    stub up snippy

--- js/project-data.js
+++ js/project-data.js
@@ -5,13 +5,7 @@ let projectData = [
     image: "./assets/Unit2Project.png",
     github: "https://github.com/kawhite8503/travel-prep-app-unit2",
     description:"An app designed to help you plan your adventures! From Event details to packing lists you can check off in real time, this app has everything you need in one convenient location!"
-  }, {
-    title: 'Future project',
-    deployment: "",
-    image: "./assets/handpic.png",
-    github: "",
-    description:"Magnificent project"
-  },
+  }, 
   {
     title: "Tic Tac Toe",
     deployment: "https://ttt-weekend-kawhite8503.netlify.app/",
@@ -25,6 +19,20 @@ let projectData = [
     image: "",
     github: "https://github.com/kawhite8503/socialfx-front-end",
     description: "A MERN stack planning app for social gatherings!"
+  },
+  {
+    title: 'Snippy (hackathon)'
+    deployment: "https://sei-snippy.netlify.app/",
+    image: "./assets/SNIPPY.png",
+    github: "",
+    description: "Best Group Project winner for hackathon. This app was designed to allow you to take notes while viewing lecture recordings, and store those notes with the specific recording."
+  }
+  {
+    title: 'Future project',
+    deployment: "",
+    image: "./assets/handpic.png",
+    github: "",
+    description:"Magnificent project"
   }
 ]
 
