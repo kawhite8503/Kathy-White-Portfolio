61551d77bcc5d3aa35950748ca9df26391660175
Kathy White <kawhite8503@gmail.com>
2022-08-17 10:27:19 -0500

    add divs in cards

--- js/app.js
+++ js/app.js
@@ -8,10 +8,14 @@ let projectMarkup = projectData.map(project =>
 <div class="card" style="width: 18rem;">
   <img src="${project.image}" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">${project.title}</h5>
-    <p class="card-text">${project.description}</p>
-    <a href="${project.github}" target="_blank" class="btn btn-primary">GitHub</a>
-    <a href="${project.deployment}" target="_blank" class="btn btn-secondary">Deployment</a>
+    <div>
+      <h5 class="card-title">${project.title}</h5>
+      <p class="card-text">${project.description}</p>
+    </div>
+    <div>
+      <a href="${project.github}" target="_blank" class="btn btn-primary">GitHub</a>
+      <a href="${project.deployment}" target="_blank" class="btn btn-secondary">Deployment</a>
+    </div>
   </div>
 </div>
   `).join(' ')
