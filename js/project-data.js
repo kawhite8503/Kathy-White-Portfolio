512025f8f314fb603cb95364378b74c4ef2bfc39
Kathy White <kawhite8503@gmail.com>
2022-08-19 08:47:46 -0500

    update work

--- js/project-data.js
+++ js/project-data.js
@@ -7,20 +7,6 @@ let projectData = [
     description:"An app designed to help you plan your adventures! From Event details to packing lists you can check off in real time, this app has everything you need in one convenient location!"
   }, 
   {
-    title: "Tic Tac Toe",
-    deployment: "https://ttt-weekend-kawhite8503.netlify.app/",
-    image: "./assets/tictactoe.png",
-    github: "",
-    description:"Who will prevail? X or O?"
-  },
-  // {
-  //   title: "Social (fx)",
-  //   deployment: "",
-  //   image: "",
-  //   github: "https://github.com/kawhite8503/socialfx-front-end",
-  //   description: "A MERN stack planning app for social gatherings!"
-  // },
-  {
     title: 'Snippy',
     deployment: "https://sei-snippy.netlify.app/",
     image: "./assets/SNIPPY.png",
@@ -28,14 +14,27 @@ let projectData = [
     description: "Best Group Project winner for GA's 2.5 day hackathon. This app was designed to allow you to take notes while viewing lecture recordings, and store those notes with the specific recording."
   },
   {
-    title: 'Future project',
-    deployment: "",
-    image: "./assets/handpic.png",
+    title: 'Chore Board',
+    deployment: "https://kwhitechoreboard.herokuapp.com/",
+    image: "./assets/Screen Shot 2022-08-19 at 8.06.19 AM.png",
+    github: "https://github.com/kawhite8503/choreboard",
+    description:"Welcome to Chore Board, an app to help organize and balance responsibilities within the home! Written in Python, utilizing PostgreSQL and Django."
+  },
+  {
+    title: "Social (fx)",
+    deployment: "https://social-fx.netlify.app/",
+    image: "./assets/Screen Shot 2022-08-18 at 4.24.15 PM.png",
+    github: "https://github.com/kawhite8503/socialfx-front-end",
+    description: "A MERN stack planning app for social gatherings! View and edit the Guest List, Activities planned, and Foods & Supplies. Utilize the Comments section to communicate with other guests!"
+  },
+  {
+    title: "Tic Tac Toe",
+    deployment: "https://ttt-weekend-kawhite8503.netlify.app/",
+    image: "./assets/tictactoe.png",
     github: "",
-    description:"Magnificent project"
-  }
+    description:"Who will prevail? X or O?"
+  },
 ]
-
 
 export {
   projectData, skills
